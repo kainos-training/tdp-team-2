85e4ca151f392b0783af06ebf51f7cd754030a24
Justin Graham <j.graham2@kainos.com>
2015-09-09 15:17:23 +0100

    add initial insert statment. create db connection class.

--- src/main/java/com/kainos/librarysystem/resource/ViewsResource.java
+++ src/main/java/com/kainos/librarysystem/resource/ViewsResource.java
@@ -1,5 +1,10 @@
 package com.kainos.librarysystem.resource;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import io.dropwizard.views.View;
 
 import javax.ws.rs.GET;
@@ -9,6 +14,7 @@ import javax.ws.rs.core.MediaType;
 
 import com.codahale.metrics.annotation.Timed;
 import com.kainos.librarysystem.views.Index;
+
 
 @Path("/")
 public class ViewsResource {
@@ -22,5 +28,46 @@ public class ViewsResource {
 	public View sayHello(){
 		return new Index();
 	}
-
+	
+	public void addBook(String title, String author, String year, String catId) throws SQLException {
+		
+		if(title.isEmpty() || author.isEmpty())
+		{
+			// error 	
+		}
+		
+		 String dbDriver = "com.mysql.jdbc.Driver";
+	     String dbUrl = "jdbc:mysql://localhost/LibraryDb";
+	     try {
+			Class.forName(dbDriver).newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	     
+		 Connection conn = DriverManager.getConnection(dbUrl, "root", "ch@ngeme1");
+		  
+		 // the mysql insert statement
+	      String query = " insert into Book (title, author, year)"
+	        + " values (?, ?, ?, ?)";
+	 
+	      // create the mysql insert preparedstatement
+	      PreparedStatement preparedStmt = conn.prepareStatement(query);
+	      preparedStmt.setString (1, title);
+	      preparedStmt.setString (2, author);
+	      preparedStmt.setString (3, year);
+	      
+	      // execute the preparedstatement
+	      preparedStmt.execute();
+	      
+	      // close con
+	      conn.close();	
+	}
+	
 }
