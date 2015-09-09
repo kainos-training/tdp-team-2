b7bfed221c2a450d583762f1384c7c30a9845008
Stefan Totev <s.totev@kainos.com>
2015-09-09 14:08:46 +0100

    Add Book class

---
+++ src/main/java/com/kainos/librarysystem/model/Book.java
@@ -0,0 +1,46 @@
+package com.kainos.librarysystem.model;
+
+public class Book {
+	
+	private String title, author;
+	private int yearPublished;
+	
+	public Book(){
+	}
+	
+	public Book(String title, String author){
+		this.title=title;
+		this.author=author;
+	}
+	
+	public Book(String title, String author, int yearPublished){
+		this.title=title;
+		this.author=author;
+		this.yearPublished=yearPublished;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public int getYearPublished() {
+		return yearPublished;
+	}
+	
+	public void setYearPublished(int yearPublished) {
+		this.yearPublished = yearPublished;
+	}
+
+}
