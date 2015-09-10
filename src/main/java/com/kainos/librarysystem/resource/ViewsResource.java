1a44b65d027686f6d6e984159ba9da218f4cd06c
joannec <j.connor@kainos.com>
2015-09-10 11:19:20 +0100

    Add ViewResourceTest and Mockito to pom

--- src/main/java/com/kainos/librarysystem/resource/ViewsResource.java
+++ src/main/java/com/kainos/librarysystem/resource/ViewsResource.java
@@ -2,19 +2,28 @@ package com.kainos.librarysystem.resource;
 
 import io.dropwizard.views.View;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.codahale.metrics.annotation.Timed;
-import com.kainos.librarysystem.views.Index;
+import com.kainos.librarysystem.database.LibraryConnector;
+import com.kainos.librarysystem.model.Book;
 import com.kainos.librarysystem.views.Home;
+import com.kainos.librarysystem.views.Index;
 
 @Path("/")
 public class ViewsResource {
 	
-	public ViewsResource(){ }
+	LibraryConnector dbConnector;
+	
+	public ViewsResource(LibraryConnector dbConnector){ 
+		this.dbConnector = dbConnector;
+	}
 	
 	@GET
 	@Timed
@@ -29,9 +38,11 @@ public class ViewsResource {
 	@Timed
 	@Path("/Home")
 	@Produces(MediaType.TEXT_HTML)
-	public View Home(){
+	public View Home() throws SQLException{
 		
-		return new Home();
+		List<Book> books = dbConnector.getBooks();
+		
+		return new Home(books);
 	
 	}
 }
---
+++ src/test/java/com/kainos/librarysystem/resource/ViewsResourceTest.java
@@ -0,0 +1,34 @@
+package com.kainos.librarysystem.resource;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.kainos.librarysystem.database.LibraryConnector;
+
+public class ViewsResourceTest {
+	
+	private LibraryConnector dbMockClient;
+	private ViewsResource viewsResource;
+	
+	@Before
+	public void setup()
+	{
+		dbMockClient = mock(LibraryConnector.class);
+		viewsResource = new ViewsResource(dbMockClient);
+	}
+	
+	@Test
+	public void testDatabaseConnectorCalledOnViewBooks() throws SQLException
+	{
+		viewsResource.Home();
+		
+		verify(dbMockClient).getBooks();
+	
+	}
+	
+}
