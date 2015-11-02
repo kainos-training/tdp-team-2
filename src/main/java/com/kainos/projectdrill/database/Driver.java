b44126117a2c4208c34ddbf75c3594ee84e0e226
CameronMcAuley <c.mcauley@kainos.com>
2015-11-02 15:40:51 +0000

    Added method for listing frameworks in database

--- src/main/java/com/kainos/projectdrill/database/Driver.java
+++ src/main/java/com/kainos/projectdrill/database/Driver.java
@@ -3,6 +3,9 @@ package com.kainos.projectdrill.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class Driver {
@@ -22,5 +25,24 @@ public class Driver {
 			e.printStackTrace();
 			System.exit(1);
 		}
-}
 	}
+	
+	
+	//Method to list all frameworks in database
+	public void listFrameworks(){
+
+		try {
+			Statement create = connection.createStatement();
+			ResultSet result = create.executeQuery("Select * from frameworks");
+			
+			while(result.next()){
+				System.out.println(result.getString(2));
+			}
+		} catch (SQLException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
