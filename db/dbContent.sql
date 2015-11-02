22cc6e83f9ea061e130e68e6c086040ebabaccdb
nathanaelh7558 <nathanaelh7558@gmail.com>
2015-11-02 15:28:18 +0000

    added language table, language foreign key also in framework table

--- db/dbContent.sql
+++ db/dbContent.sql
@@ -2,22 +2,29 @@
 -- Insert into frameworks table
 
 USE team2;
-call addFramework("Grails","Java");
-call addFramework("Play", "Java");
-call addFramework("SpringMVC", "Java");
-call addFramework("Dropwizard", "Java");
-call addFramework("ASP.NET", "C#");
-call addFramework("AngularJS", "Javascript");
-call addFramework("KnockoutJS", "Javascript");
-call addFramework("Backbone.js", "JavaScript");
-call addFramework("Ember.js", "Javascript");
-call addFramework("Express.js", "JavaScript");
-call addFramework("Unified.js", "Javascript");
-call addFramework("Play", "Scala");
-call addFramework("Zend Framework", "PHP");
-call addFramework("Sinatra", "Ruby");
-call addFramework("Ruby on Rails", "Ruby");
-call addFramework("Drupal", "PHP");
+CALL addLanguage("Java");
+CALL addLanguage("C#");
+CALL addLanguage("Javascript");
+CALL addLanguage("Scala");
+CALL addLanguage("PHP");
+CALL addLanguage("Ruby");
+
+call addFramework("Grails",1);
+call addFramework("Play", 1);
+call addFramework("SpringMVC", 1);
+call addFramework("Dropwizard", 1);
+call addFramework("ASP.NET", 2);
+call addFramework("AngularJS", 3);
+call addFramework("KnockoutJS", 3);
+call addFramework("Backbone.js", 3);
+call addFramework("Ember.js", 3);
+call addFramework("Express.js", 3);
+call addFramework("Unified.js", 3);
+call addFramework("Play", 4);
+call addFramework("Zend Framework", 5);
+call addFramework("Sinatra",6);
+call addFramework("Ruby on Rails", 6);
+call addFramework("Drupal", 5);
 
 
 
--- db/dbSchema.sql
+++ db/dbSchema.sql
@@ -3,18 +3,32 @@ CREATE DATABASE team2;
 
 USE team2;
 
+CREATE TABLE languages (
+ID int(11) PRIMARY KEY Auto_INCREMENT,
+Name varchar(50)
+);
 CREATE TABLE frameworks (
 ID int(11) PRIMARY KEY AUTO_INCREMENT,
 Name varchar(50),
-Language varchar(50)
+Language int(11), FOREIGN KEY (Language) 
+	REFERENCES languages(ID) 
 );
+
 DELIMITER // 
 CREATE PROCEDURE addFramework (name varchar(50),
-language varchar(50)
+languageId int(11)
 	)
 BEGIN
 INSERT INTO frameworks(Name, Language)
-VALUES (name, language);
+VALUES (name, languageId);
 end //
 DELIMITER ;
 
+DELIMITER // 
+CREATE PROCEDURE addLanguage (name varchar(50)
+	)
+BEGIN
+INSERT INTO languages(Name)
+VALUES (name);
+end //
+DELIMITER ;
