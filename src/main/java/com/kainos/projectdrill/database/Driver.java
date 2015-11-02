8ff575a6cf30a806335ddc15f2d4371fd9038a43
CameronMcAuley <c.mcauley@kainos.com>
2015-11-02 15:25:54 +0000

    Created initial setup file for jdbc database driver

---
+++ src/main/java/com/kainos/projectdrill/database/Driver.java
@@ -0,0 +1,26 @@
+package com.kainos.projectdrill.database;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class Driver {
+	private final String databaseUrl = "jdbc:mysql://localhost";
+	private final String databaseUser = "root";
+	private final String databasePassword = "ch@ngeme1";
+	Connection connection;
+
+	
+	//Try to connect to database
+	public void connectToDatabase(){
+		try{
+		    connection = DriverManager.getConnection(databaseUrl,databaseUser,databasePassword);
+		    System.out.println("Successfully connected to database.");
+		} catch (Exception e){
+			System.out.println("Error occured while attempting connection to database");
+			e.printStackTrace();
+			System.exit(1);
+		}
+}
+	}
